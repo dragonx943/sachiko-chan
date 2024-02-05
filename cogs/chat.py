cd1ff2027278ff072eabf3583f68f4ffd8bf7137
dragonx943 <premieregirl26@gmail.com>
2024-02-05 21:44:17 +0700

    Fix bug Windows & Linux

---
+++ cogs/chat.py
@@ -0,0 +1,54 @@
+from discord.ext.commands import Cog
+import discord
+
+import utilities
+
+class Chat(Cog):
+    def __init__(self, bot: discord.Bot):
+        self.bot: discord.Bot = bot
+        self.conversations: dict = {}
+
+    @Cog.listener()
+    async def on_message(self, message: discord.Message):
+        # ignore messages sent by the bot
+        if message.author == self.bot.user:
+            return
+
+        # ignore messages that aren't mentioning the bot
+        if not utilities.is_mentioned(self.bot.user, message):
+            return
+
+        author = str(message.author.id)
+
+        content = message.clean_content.replace(f"@{self.bot.user.name}", "")
+        content = content.strip()
+
+        if author not in self.conversations:
+            self.conversations[author] = [{"role": "user", "content": self.bot.initial_prompt}]
+
+        self.conversations[author].append({
+            "role": "user", "content": content
+        })
+
+        with message.channel.typing():
+            response = await utilities.chat_request(self.conversations[author])
+
+        content = utilities.filter_markdown(response.content)
+
+        # append the bot's response to the conversation
+        self.conversations[author].append(response)
+
+        # send the bot's response
+        await message.reply(content, mention_author=False)
+
+    @discord.slash_command(description="Tẩy não Sachiko-chan (Trong trường hợp câu từ của cô ấy mất kiểm soát)")
+    async def forget(self, ctx: discord.ApplicationContext):
+        author = str(ctx.author.id)
+
+        if author in self.conversations:
+            self.conversations[author] = [{"role": "user", "content": self.bot.initial_prompt}]
+
+        await ctx.respond("`Đã dùng phép thuật tẩy não Sachiko-chan thành công thông qua ChatGPT API. Tag cô ấy = Tạo 1 cuộc trò chuyện mới!`")
+
+def setup(bot: discord.Bot):
+    bot.add_cog(Chat(bot))
---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+import openai
+import dotenv
+
+import logging
+import os
+
+# load the .env file
+dotenv.load_dotenv()
+
+# set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="(%(asctime)s) [%(levelname)s] %(message)s",
+    datefmt="%H:%M:%S",
+    
+    handlers=[
+        logging.FileHandler(filename="discord.log", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+# initialize the OpenAI API
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# initialize the Discord bot
+intents = discord.Intents.default()
+
+intents.message_content = True
+intents.members = True
+
+bot = discord.Bot(intents=intents)
+
+# set variables in the bot
+with open("prompt.txt", "r", encoding="utf-8") as file:
+    bot.initial_prompt = file.read()
+
+# load the bot's cogs
+bot.load_extension("cogs.events")
+bot.load_extension("cogs.chat")
+bot.load_extension("cogs.admin")
+
+# run the bot
+bot.run(os.getenv("DISCORD_TOKEN"))
